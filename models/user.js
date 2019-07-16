d61d3fc9273d7b07c5afcbc5bb26d84d00eba5b1
franklin fargoj <franklin.fargoj@neosofttech.com>
2019-07-16 17:57:41 +0530

    uniqueValidator

--- models/user.js
+++ models/user.js
@@ -1,14 +1,17 @@
 var mongoose = require('mongoose');
 var passport = require("passport");
 var bcrypt = require('bcrypt');
+var uniqueValidator = require('mongoose-unique-validator');
 
 //Schema for users collection
 var UserSchema = new mongoose.Schema({
-    email : {type:String, require:true},
+    email : {type:String,unique: true,require:true},
     username: {type:String, require:true},
     password:{type:String, require:true},
     creation_dt:{type:Date, require:true}
   },{ versionKey: '' }, { collection: 'users' });
+
+  UserSchema.plugin(uniqueValidator);
 
   UserSchema.statics.hashPassword = function hashPassword(password){
     return bcrypt.hashSync(password,10);
